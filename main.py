f45593e878fedf9394b226c67b7486d02d8bc7cc
nguyntinkhang <116292903+Compscicafe@users.noreply.github.com>
2024-03-24 22:58:52 +0700

    Initilize the notebook and basic file reading

---
+++ main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# Specify the file path
+file_path = 'All_GPUs.csv'
+
+# Load the CSV file into a pandas DataFrame
+data = pd.read_csv(file_path)
+
+# Print the loaded data
+#print(data)
+
+data.dropna(inplace=True)
+print(data)
+
+
